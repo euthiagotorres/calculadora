d9b1324ee8b95229f0f5c488a7569e3a8689df84
euthiagotorres <thiagot777@gmail.com>
2024-05-20 08:53:03 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+num1=float(input('Digite o primeiro número:'))
+num2=float(input('Digite o segundo número:'))
+soma=num1+num2
+subt=num1-num2
+mult=num2*num2
+div=num1/num2
+print("a soma entre os dois números é:{}".format(soma))
+print("a subtração entre os dois números é:{}".format(subt))
+print("a multiplicação entre os dois números é:{}".format(mult))
+print("a divisão entre os dois números é:{}".format(div))
